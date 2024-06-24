2148f7fb3850928eb41f11a7fb9a3ae8a5dc828c
Milosz11 <mildab141@gmail.com>
2024-06-23 22:23:46 -0500

    Complete business card

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,14 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +44,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BusinessCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BusinessCard()
                 }
             }
         }
@@ -31,17 +52,108 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun BusinessCard(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.android_logo)
+    val name = stringResource(R.string.name)
+    val title = stringResource(R.string.title)
+    val phoneNumber = stringResource(R.string.phone_number)
+    val email = stringResource(R.string.email)
+
+    Column(
+        modifier = modifier.fillMaxSize().background(Color(0xFF936AD6)),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Header(
+            image = image,
+            name = name,
+            title = title
+        )
+        ContactFooter(
+            phoneNumber = phoneNumber,
+            email = email
+        )
+    }
+}
+
+@Composable
+fun Header(image: Painter, name: String, title: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+            .padding(horizontal = 16.dp)
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = modifier
+                .height(192.dp)
+                .width(192.dp)
+        )
+        Text(
+            text = name,
+            fontWeight = FontWeight.Bold,
+            fontSize = 36.sp,
+            color = Color(0xFF3ddc84),
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun ContactFooter(phoneNumber: String, email: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Icon(
+                Icons.Rounded.Call,
+                contentDescription = null,
+                modifier = Modifier.padding(end = 8.dp),
+                tint = Color(0xFF3ddc84)
+            )
+            Text(
+                text = phoneNumber,
+                fontSize = 16.sp
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Icon(
+                Icons.Rounded.Email,
+                contentDescription = null,
+                modifier = Modifier.padding(end = 8.dp),
+                tint = Color(0xFF3ddc84)
+            )
+            Text(
+                text = email
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
